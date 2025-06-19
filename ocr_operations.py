a46d7a031f9531be6f013526ce17643c63b2c062
wateen23 <zahranwateen@gmail.com>
2025-06-19 17:09:20 +0300

    claims almost done

--- ocr_operations.py
+++ ocr_operations.py
@@ -1,16 +1,30 @@
-import pdfplumber 
+import pdfplumber
 import pandas as pd
+import re
+
 
 class PDFExtractor:
-    def __init__(self, file_path:str):
+    def __init__(self, file_path: str):
         self.path = file_path
 
+    def turn_meta_data_to_dict(self, table: list[list[str]]) -> dict:
+        meta_data_dict = {}
+        for row in table:
+            for i in range(0, len(row) - 1, 2):
+                key = row[i]
+                value = row[i + 1]
+                meta_data_dict[key] = value
+        return meta_data_dict
+
+    def clean_headers(self, name: str) -> str:
+        seprated_name = re.sub(r"(?<=[a-zA-Z])of(?=[A-Z])", " of ", name)
+        # name = re.sub(r'(?<!^)(?=[A-Z])', '', name)
+        seprated_name = seprated_name.replace("\n", " ")
+        print(seprated_name)
+        return seprated_name
+
     def extract(self) -> dict:
-        result = {
-            "meta_data": [],
-            "claims": [],
-            "benefits": []
-        }
+        result = {"meta_data": [], "claims": [], "benefits": []}
 
         with pdfplumber.open(self.path) as pdf:
             for page in pdf.pages:
@@ -20,9 +34,11 @@ class PDFExtractor:
                         continue
 
                     first_row = " ".join(str(cell).lower() for cell in table[0] if cell)
-        
+
                     if "groupnumber" in first_row:
-                        result["meta_data"].extend(table)
+                        meta_data_dict = self.turn_meta_data_to_dict(table)
+                        # print(f"\n\n{meta_data_dict}\n\n")
+                        result["meta_data"].extend(meta_data_dict)
 
                     elif "monthlyclaims" in first_row:
                         result["claims"].extend(table)
@@ -32,10 +48,45 @@ class PDFExtractor:
 
         return result
 
-    def preprocess(self, tables_dict:dict):
-        pass
+    def preprocess_claims_df(self, tables_dict: dict):
+        meta_data = tables_dict["meta_data"]
+        raw_df = pd.DataFrame(tables_dict["claims"])
+
+        old_col = raw_df.iloc[0]
+        new_col = [self.clean_headers(col_name) for col_name in old_col]
+        df = raw_df[1:].copy()
+        df.columns = new_col
+
+        policy_year = None
+        cleaned_rows = []
+
+        for _, row in df.iterrows():
+            first_col = str(row[new_col[0]]).strip().lower()
+
+            if "2year" in first_col:
+                policy_year = "2 Years Prior"
+                continue
+            elif "priorpolicyyear" in first_col:
+                policy_year = "Prior Policy Year"
+                continue
+            elif "lastpolicyyear" in first_col:
+                policy_year = "Last Policy Year"
+                continue
+            elif "overall" in first_col:
+                continue
+            elif set(row.dropna().unique()) == {"0"}:
+                continue
+
+        row["Policy Year"] = policy_year
+        cleaned_rows.append(row)
+
+        cleaned_df = pd.DataFrame(cleaned_rows)
+        return cleaned_df.reset_index(drop=True)
 
 
 if __name__ == "__main__":
     extractor = PDFExtractor(file_path="OCR Test Template (1) (1).pdf")
-    extractor.extract()
+    # tables = extractor.extract()
+    # extractor.preprocess_claims_df(tables)
+    x = "NumberofLives\nInsured"
+    extractor.clean_headers(x)
